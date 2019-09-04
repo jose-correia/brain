ea72465aeb91916ca82988bfa5a6ecdaf27e5c7d
jose.correia <jose.meneses.correia@unbabel.com>
2019-09-05 00:56:08 +0100

    fixed create user servicce

--- jeec_brain/services/users/create_user_service.py
+++ jeec_brain/services/users/create_user_service.py
@@ -1,14 +1,16 @@
 from jeec_brain.models.users import Users
 
+
 class CreateUserService():
+
     def __init__(self, username, role):
         self.username = username
         self.role = role
 
-    def call(self):
-        user = Users(username=self.username, role=self.role)
+    def call(self) -> Optional[Users]:
+        user = Users.create(username=self.username, role=self.role)
         
-        user.create()
-        user.reload()
-        
+        if not user:
+            return None
+
         return user
