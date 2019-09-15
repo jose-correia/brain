326d960c185b9ed276be016515d1879196fc0e44
jose.correia <jose.meneses.correia@unbabel.com>
2019-09-15 19:13:43 +0100

    Fix

--- jeec_brain/apps/admin_api/routes.py
+++ jeec_brain/apps/admin_api/routes.py
@@ -25,10 +25,10 @@ def admin_login():
     if AuthHandler.login_admin(username, password) is False:
         return render_template('admin/admin_login.html', error="Invalid credentials!")
 
-    # resp = make_response(render_template('sample.html'))
-    # resp.set_cookie('args', args)
+    resp = make_response(render_template('admin/dashboard.html'))
+    resp.set_cookie('ADMIN', username)
 
-    return redirect(url_for('admin_api.dashboard'))
+    return resp
 
 
 # content routes
