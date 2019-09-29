ab082e578d7b71e4864033e981f1640d548f21d8
jose.correia <jose.meneses.correia@unbabel.com>
2019-09-29 21:39:30 +0100

    Created rename_image_service

---
+++ jeec_brain/services/files/rename_image_service.py
@@ -0,0 +1,22 @@
+import os
+from flask import current_app
+
+
+class RenameImageService(object):
+    def __init__(self, folder, old_name, new_name):
+        self.folder = folder
+        self.old_name = old_name
+        self.new_name = new_name
+
+    def call(self):
+        for extension in current_app.config['ALLOWED_IMAGES']:
+            old_image_filename = self.old_name.lower().replace(' ', '_') + '.' + extension
+            old_file = os.path.join(current_app.root_path, self.folder, old_image_filename)
+
+            new_image_filename = self.new_name.lower().replace(' ', '_') + '.' + extension
+            new_file = os.path.join(current_app.root_path, self.folder, new_image_filename)
+
+            os.rename(old_file, new_file)
+
+            return True
+        return False
