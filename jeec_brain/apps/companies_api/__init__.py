137150c87ebe9b1d355965eebe365646ecaa76c9
jose.correia <jose.meneses.correia@unbabel.com>
2019-10-17 15:52:40 +0100

    Added companies_api

---
+++ jeec_brain/apps/companies_api/__init__.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+bp = Blueprint('companies_api', __name__)
+
+from jeec_brain.apps.companies_api import routes
+from jeec_brain.apps.companies_api.auctions import routes
+from jeec_brain.apps.companies_api.resumes import routes
---
+++ jeec_brain/apps/companies_api/routes.py
@@ -0,0 +1,41 @@
+from flask import request, render_template, redirect, url_for
+from jeec_brain.apps.companies_api import bp
+
+from flask_login import current_user
+from jeec_brain.apps.auth.wrappers import require_company_login
+from jeec_brain.apps.auth.handlers.auth_handler import AuthHandler
+
+
+@bp.route('/', methods=['GET'])
+def get_company_login_form():
+    if current_user.is_authenticated:
+        return redirect(url_for('company_api.dashboard'))
+
+    return render_template('companies/companies_login.html')
+
+
+@bp.route('/', methods=['POST'])
+def company_login():    
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if AuthHandler.login_company(username, password) is False:
+        return render_template('companies/companies_login.html', error="Invalid credentials!")
+
+    return redirect(url_for('companies_api.dashboard'))
+
+
+@bp.route('/company-logout', methods=['GET'])
+def company_logout():
+    try:
+        AuthHandler.logout_company()
+    except:
+        pass
+    return redirect(url_for('companies_api.get_company_login_form'))
+
+
+@bp.route('/dashboard', methods=['GET'])
+@require_company_login
+def dashboard():
+    return render_template('companies/dashboard.html')
+
