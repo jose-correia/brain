5ba25fb0f60bb8a693b9738ce86cb902c3d9e1b5
pfernandes21 <pedro.miguel.fernandes@tecnico.ulisboa.pt>
2020-02-08 21:50:34 +0000

    added delete and update speaker and company activities service calls

--- jeec_brain/handlers/activities_handler.py
+++ jeec_brain/handlers/activities_handler.py
@@ -3,7 +3,11 @@ from jeec_brain.services.activities.create_activity_service import CreateActivit
 from jeec_brain.services.activities.update_activity_service import UpdateActivityService
 from jeec_brain.services.activities.delete_activity_service import DeleteActivityService
 from jeec_brain.services.activities.add_company_activity_service import AddCompanyActivityService
+from jeec_brain.services.activities.update_company_activities_service import UpdateCompanyActivityService
+from jeec_brain.services.activities.delete_company_activities_service import DeleteCompanyActivityService
 from jeec_brain.services.activities.add_speaker_activity_service import AddSpeakerActivityService
+from jeec_brain.services.activities.update_speaker_activities_service import UpdateSpeakerActivityService
+from jeec_brain.services.activities.delete_speaker_activities_service import DeleteSpeakerActivityService
 
 
 class ActivitiesHandler():
@@ -25,5 +29,21 @@ class ActivitiesHandler():
         return AddSpeakerActivityService(speaker.id, activity.id).call()
 
     @classmethod
+    def update_speaker_activity(cls, speaker_activity, speaker, activity):
+        return UpdateSpeakerActivityService(speaker_activity, speaker.id, activity.id).call()
+
+    @classmethod
+    def delete_speaker_activities(cls, speaker_activity):
+        return DeleteSpeakerActivityService(speaker_activity).call()
+
+    @classmethod
     def add_company_activity(cls, company, activity):
         return AddCompanyActivityService(company.id, activity.id).call()
+
+    @classmethod
+    def update_company_activity(cls, company_activity, company, activity):
+        return UpdateCompanyActivityService(company_activity, company.id, activity.id).call()
+
+    @classmethod
+    def delete_company_activities(cls, company_activity):
+        return DeleteCompanyActivityService(company_activity).call()
