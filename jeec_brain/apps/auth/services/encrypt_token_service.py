c35fffa008906f399b61e099c55d7fcf6637006f
pfernandes21 <pedro.miguel.fernandes@tecnico.ulisboa.pt>
2021-03-01 22:50:59 +0000

    change encryption service parameter name

--- jeec_brain/apps/auth/services/encrypt_token_service.py
+++ jeec_brain/apps/auth/services/encrypt_token_service.py
@@ -9,13 +9,13 @@ logger = logging.getLogger(__name__)
 class EncryptTokenService(object):
 
     @classmethod
-    def __init__(self, fenix_auth_code):
-        self.fenix_auth_code = fenix_auth_code
+    def __init__(self, plaintext):
+        self.plaintext = plaintext
 
     def call(self):
         master_key = b'12345678901234561234567890123456'         
         encoder = PKCS7Encoder()
-        raw = encoder.encode(self.fenix_auth_code)
+        raw = encoder.encode(self.plaintext)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new( master_key, AES.MODE_CBC, iv )
         encrypted_text = base64.b64encode(iv + cipher.encrypt(str.encode(raw))) 
