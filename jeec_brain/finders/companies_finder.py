1d53fdc99dd3e03adaca983dd0354243f59267f9
pfernandes21 <pedro.miguel.fernandes@tecnico.ulisboa.pt>
2021-03-11 22:54:27 +0000

    fixed companies finder bug

--- jeec_brain/finders/companies_finder.py
+++ jeec_brain/finders/companies_finder.py
@@ -1,6 +1,7 @@
 from jeec_brain.models.companies import Companies
 from jeec_brain.models.company_activities import CompanyActivities
 from jeec_brain.models.activities import Activities
+from jeec_brain.models.activity_types import ActivityTypes
 from jeec_brain.models.events import Events
 from jeec_brain.database import db_session
 from sqlalchemy import text
@@ -40,7 +41,7 @@ class CompaniesFinder():
 
     @classmethod
     def get_companies_from_default_event(cls):
-        return Companies.query.filter((Companies.id == CompanyActivities.company_id) & (Activities.id == CompanyActivities.activity_id) & (Activities.event_id == Events.id) & (Events.default == True)).order_by(Companies.name).all()
+        return Companies.query.filter((Companies.partnership_tier != "sponsor") & (Companies.id == CompanyActivities.company_id) & (Activities.id == CompanyActivities.activity_id) & (Activities.event_id == Events.id) & (Events.default == True)).order_by(Companies.name).all()
     
     @classmethod
     def get_companies_from_event(cls, event):
@@ -56,7 +57,7 @@ class CompaniesFinder():
         day = now.strftime('%d %b %Y, %a')
         time = now.strftime("%H:%M")
 
-        return Companies.query.filter((Companies.id == CompanyActivities.company_id) & (Activities.id == CompanyActivities.activity_id) & (Activities.day == day) & (Activities.time <= time) & (Activities.end_time > time) & (Activities.chat_type == 'individual') & (Activities.activity_type.name == 'Job Fair Booth') & (Activities.event_id == Events.id) & (Events.default == True)).filter_by(**kwargs).all()
+        return Companies.query.filter((Companies.id == CompanyActivities.company_id) & (Activities.id == CompanyActivities.activity_id) & (Activities.day == day) & (Activities.time <= time) & (Activities.end_time > time) & (Activities.chat_type == 'individual') & (Activities.activity_type_id == ActivityTypes.id) & (ActivityTypes.name == 'Job Fair Booth') & (Activities.event_id == Events.id) & (Events.default == True)).filter_by(**kwargs).all()
     
     @classmethod
     def get_company_auctions(cls, company):
